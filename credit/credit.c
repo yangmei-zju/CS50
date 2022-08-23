9ec21101e449d5ce15a303c94a534f187999f333
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-08-23 11:58:35 +0000

    Tue, Aug 23, 2022, 11:58 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -8,13 +8,19 @@ int main(void)
     number = get_long();
     }while(number>0);
 
-    int sum = 0;
+    int sum1 = 0;
+    int sum2 = 0;
     int a = 0;
-    int b = 0;
-    for(i = 0;number>0;i++){
+    int sum = 0;
+    for(i = 1;number>0;i++){
         a = number%10;
         number = number/10;
-        if(i)
+        if(i%2==0)
+        {
+            sum1+ = (a*2);
+        }else
+            sum2+ = a;
+
     }
 
 
