8f39499c5d0e52499c1f815211c5a95ee10ec898
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-08-24 02:14:42 +0000

    Wed, Aug 24, 2022, 2:14 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -13,13 +13,14 @@ int main(void)
     int sum2 = 0;
     int a = 0;
     int sum = 0;
-    for(int i = 1,number = Number;number>0;i++)
+    for(int i = 1,int number = 0;number>0;i++)
     {
+        number = Number;
         a = number%10;
         number = number/10;
         if(i%2==0)
         {
-            sum1+ = (a*2);
+            sum1 + = (a*2);
         }
         else
             sum2 + = a;
@@ -35,9 +36,6 @@ int main(void)
     }
     else
     {
-
-
-
 
     int b = Number;
     while( b>10&&b<100)
