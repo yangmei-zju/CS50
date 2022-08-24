8abdfe54a1b5ac5810a8e9829cfb0b7aafc2491a
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-08-24 10:50:41 +0000

    Wed, Aug 24, 2022, 10:50 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -60,7 +60,8 @@ int main(void)
         {
             b = b%10;
         }
-        //根据b判断credit类型
+        printf("%ld\n",b);
+        /*//根据b判断credit类型
         if((b == 34 || b == 37)&& length == 15)
         {
             printf("AMEX\n");
@@ -80,7 +81,7 @@ int main(void)
     }
     else
     {
-        printf("INVALID\n");
+        printf("INVALID\n");*/
 
     }
 }
