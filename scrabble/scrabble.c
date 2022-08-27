51dbfaad2b5932bc96a9d2b2323b96cd526c972d
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-08-27 09:52:00 +0000

    Sat, Aug 27, 2022, 9:52 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,6 +19,14 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if(score1 > score2)
+    {
+        printf("Player1 wins!");
+    }
+    else
+    {
+        printf("Player2 wins!");
+    }
 }
 
 int compute_score(string word)
