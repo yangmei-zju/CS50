69c2d5c5408e683be43390dbbb2a4a0b1037e186
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-08-27 11:26:10 +0000

    Sat, Aug 27, 2022, 11:26 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -39,7 +39,7 @@ int compute_score(string word)
     int score = 0;
     for(int i = 0, len = strlen(word);i < len;i++)
     {
-        if (issupper(word[i]))
+        if (isupper(word[i]))
         {
             score += POINTS[word[i] - 'A'];
         }
