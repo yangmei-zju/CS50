a10d2134f51abdf3436eb0b6a7c048b6e37bfdbd
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-09-14 15:14:37 +0000

    Wed, Sep 14, 2022, 3:14 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -2,13 +2,86 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+int space(string argv);
+int alpha(string argv);
+int repeat(string argv);
+
 int main(int argc, string argv[])
 {
-    if(strlen(argv) != 26)
+    if(strlen(argv) == 0)
     {
-        printf(")
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+    else if(!space(argv) )
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+    else if(strlen(argv) != 26)
+    {
+        printf("Key must contain 26 characters.\n");
+        return 1;
+    }
+    else if(! alpha(argv))
+    {
+        printf("Key must only contain alphabetic characters.\n");
+        return 1;
+    }
+    else if(! repeat(argv))
+    {
+        printf("Key must not contain repeated characters.\n");
+        return 1;
     }
 
+   /*string plaintext = get_string("plaintext:");*/
 
 
+
+
+
+}
+
+int space(string argv)
+{
+    int i = 0;
+    int n = 0;
+    for(i = 0;i < strlen(argv);i ++)
+    {
+        if(isspace(argv[i]))
+        {
+            n++;
+        }
+    }
+    return n;
+}
+int alpha(string argv)
+{
+    int i = 0;
+    int n = 26;
+    for(i=0;argv[i] != '/0';i++)
+    {
+        if(isalpha(argv[i]))
+        {
+            n--;
+        }
+    }
+    return n;
+}
+int repeat(string argv)
+{
+    int n = 0;
+    for(int i = 1;argv[i] != '/0';i++)
+    {
+        for(int j = 0; j < i;j++)
+        {
+            if(argv[j] == argv[i])
+            {
+                n++;
+            }
+        }
+
+
+    }
+    return n;
 }
