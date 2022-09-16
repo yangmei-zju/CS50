fb8f39ce6f3304343f0a101f2df736c5879dc517
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-09-16 11:01:05 +0000

    Fri, Sep 16, 2022, 11:01 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -31,7 +31,7 @@ int main(int argc, string argv[])
     }
 
    string plaintext = get_string("plaintext:");
-   string ciphertext ;
+   string ciphertext = NULL;
    strcpy(plaintext,ciphertext);
 
    /*for(int i = 0;plaintext[i] != '\0';i++)
