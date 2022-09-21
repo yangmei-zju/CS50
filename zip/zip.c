e49f8ae7a6e5db6d258272122f889c4e63310bb9
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-09-21 06:53:17 +0000

    Wed, Sep 21, 2022, 6:53 AM +00:00

---
+++ zip/zip.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define MAX 10
+void zip(char *p);
+int main()
+{
+    char line[MAX];
+    gets(line);
+    zip(line);
+    puts(line);
+    return 0;
+}
+void zip(char *p)
+{
+    char *q ;
+    q = p;
+    int n ;
+    while(*p != '\0')
+    {
+        n = 1;
+        while(*p == *(p+n))
+        {
+            n++;
+        }
+        if(n >= 10)
+        {
+            *q++ = (n/10) + '0';
+            *q++ = (n%10) + '0';
+        }
+        else if(n>=2)
+        {
+            *q++ = n + '0';
+
+        }
+        *q++ = *(p+n-1);
+        p = p+n;
+    }
+    *q = '\0';
+}
