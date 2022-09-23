7dc56202af2168501bd8573b64cd29e9a6f9ee03
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-09-23 15:15:25 +0000

    Fri, Sep 23, 2022, 3:15 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,7 +66,7 @@ int main(int argc, string argv[])
 bool vote(string name)
 {
     // TODO
-    for (int i = 0; i <= candidate_count; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
         if (strcmp(name, candidates[i].name) == 0)
         {
