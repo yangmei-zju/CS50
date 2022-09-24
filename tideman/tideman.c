c9bc089baf8e73602a6c14cd64d84e08c41a0e0c
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-09-24 13:41:29 +0000

    Sat, Sep 24, 2022, 1:41 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -206,11 +206,12 @@ void print_winner(void)
 {
     // TODO
     for (int i = 0; i < candidate_count; i++)
-    {
-        for (int j = 0; j < candidate_count; j++)
-        {
-            
-        }
+    {  int j = 0;
+       while(locked[j][i] == false)
+       {
+        j++;
+        if( j == )
+       }
     }
     return;
 }
