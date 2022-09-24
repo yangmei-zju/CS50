b0981d97eb317d8f8d01e5a99c4a8d32f18c38ba
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-09-24 12:31:42 +0000

    Sat, Sep 24, 2022, 12:31 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -140,7 +140,16 @@ void add_pairs(void)
     // TODO
     for (int n =0; n < pair_count; n++)
     {
-        
+        for (int i = 0; i < candidate_count; i++)
+        {
+            for (int j = 0; j < candidate_count; j++)
+            {
+                if (preference[i][j] > preference[j][i])
+                {
+                    pair
+                }
+            }
+        }
     }
     return;
 }
