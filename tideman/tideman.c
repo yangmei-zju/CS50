d1fa358f82a5b04254f8ebf3faae5d6d9db6fb06
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-09-26 12:21:23 +0000

    Mon, Sep 26, 2022, 12:21 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -162,7 +162,7 @@ void sort_pairs(void)
     {
         for (int j = 0; j <pair_count-i-1; j++)
         {
-            if (preferences[pairs[j].winner][pairs[j].loser]/*/preferences[pairs[j].loser][pairs[j].winner])*/ < preferences[pairs[j+1].winner][pairs[j+1].loser]/*/preferences[pairs[j+1].loser][pairs[j+1].winner]))*/
+            if (preferences[pairs[j].winner][pairs[j].loser]/*/preferences[pairs[j].loser][pairs[j].winner])*/ < preferences[pairs[j+1].winner][pairs[j+1].loser]/*/preferences[pairs[j+1].loser][pairs[j+1].winner])*/)
             {
                 int t ,p;
                 t = pairs[j].winner;
