376dcbb23fcd0379fb070ed4907dfeff8dcfcbaa
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-10-05 07:38:04 +0000

    Wed, Oct 5, 2022, 7:38 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -67,8 +67,14 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
-
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            
+        }
+    }
     return;
 }
 
