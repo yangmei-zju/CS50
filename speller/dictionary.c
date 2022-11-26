1bdbde0b69929e3aad4ed959f668acf0aeb3837b
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-11-26 10:42:06 +0000

    Sat, Nov 26, 2022, 10:42 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -16,7 +16,7 @@ typedef struct node
 node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 26*4;
+const unsigned int N = 26;
 
 // Hash table
 node *table[N];
@@ -46,10 +46,7 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    int m = toupper(word[0]) - 'A';
-    int n = toupper(word[1]) - 'A';
-    int index = 4 * m + n * 26 / 4;
-    return index;
+    return toupper(word[0]) - 'A';
 }
 
 // Loads dictionary into memory, returning true if successful, else false
