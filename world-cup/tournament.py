d8143c1f8dcc82af11410ca910b04545bedcc8a6
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-11-28 11:17:20 +0000

    Mon, Nov 28, 2022, 11:17 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -26,7 +26,7 @@ def main():
     # TODO: Simulate N tournaments and keep track of win counts
     for i in range(N):
         winner = simulate_tournaments
-        if winner in count
+        if winner in count:
             count[winner] += 1
         else:
             count[winner] = 1
