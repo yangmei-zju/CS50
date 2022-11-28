80009a6f80172079bf26928861e3fa29d30dd6de
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-11-28 12:31:10 +0000

    Mon, Nov 28, 2022, 12:31 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,11 +1,20 @@
 # TODO
-height = int(input(Height:))
-while height < 0 or height > 9:
+def main():
+
     height = int(input(Height:))
-for i in range(height)
+    while height < 0 or height > 9:
+        height = int(input(Height:))
+    for i in range(1,height,1):
+        space_print()
 
    #  #
   ##  ##
  ###  ###
 ####  ####
-def space_print:
+def space_print(n):
+    for i in range(n):
+        print(" ",end = "")
+
+def hash_print(n):
+    for i in range(n):
+        print("#",end = "")
