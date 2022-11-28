e939939cbe39b56e43acf34e38cb20bac9d793c0
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-11-28 10:12:32 +0000

    Mon, Nov 28, 2022, 10:12 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -19,8 +19,7 @@ def main():
     with open(sys.argv[1]) as file:
         reader = csv.Dictreader(file)
     for row in reader:
-        team = row["team"]
-        teams[team] = 
+        teams = row["team"]
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
