8496d7cd74bc9804fc5a28b577ffba8198915f15
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-11-28 11:11:49 +0000

    Mon, Nov 28, 2022, 11:11 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -24,6 +24,10 @@ def main():
 
     count{}
     # TODO: Simulate N tournaments and keep track of win counts
+    for i in range(N)
+        team = simulate_tournaments
+        if count["team"] = 
+        count["team"] += 1
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
@@ -56,7 +60,9 @@ def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
     while len(teams) > 1:
-         teams.append(simulate_round(teams))
+         teams = simulate_round(teams)
+
+    return teams[0]["team"]
 
 
 
