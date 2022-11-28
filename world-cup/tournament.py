e8e5b33d22db26613852b1cdcddd1764106b0012
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-11-28 11:18:42 +0000

    Mon, Nov 28, 2022, 11:18 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -17,7 +17,7 @@ def main():
     teams = [] # the teams is a list of dictionary
     # TODO: Read teams into memory from file
     with open(sys.argv[1]) as file:
-        reader = csv.Dictreader(file)
+        reader = csv.DictReader(file)
     for team in reader:
         team["rating"] = int( team["rating"])
         teams.append(team)
