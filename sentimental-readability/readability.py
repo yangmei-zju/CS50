271944ddff39f46c9cd53ab12060cb20fbaa098b
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-12-02 16:05:27 +0000

    Fri, Dec 2, 2022, 4:05 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -9,17 +9,16 @@ for word in text:
         words = words + 1
     elif word == '.' or word == '?' or word == '!':
         sentences = sentences +  1
-    elif word >= 'a' or word <= 'z':
+    elif word >= 'a' and word <= 'z':
         letters = letters + 1
-    elif word >= 'A' or word <= 'Z':
+    elif word >= 'A' and word <= 'Z':
         letters = letters + 1
 
 l = letters / words * 100
 s = sentences / words * 100
-print(l)
-print(s)
+
 index = 0.0588 * l - 0.296 * s - 15.8
-index = int(index)
+index = round(index)
 if index < 1:
     print("Before Grade 1")
 elif index >= 16:
