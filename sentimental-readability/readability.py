afe6d41bc7af13acf612360896bc2f02d8bd1ec7
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-12-02 14:07:26 +0000

    Fri, Dec 2, 2022, 2:07 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -6,4 +6,9 @@ i = 0
 while true:
     if text[i] == '\0':
         break
-    
+    if text[i] == ' ':
+        words = words + 1
+    if text[i] == '.' and text[i] == '?'and text[i] == '!':
+        sentences = sentences +  1
+    i = i + 1
+
