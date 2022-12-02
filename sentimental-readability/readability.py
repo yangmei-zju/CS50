78d3c9c2c518a00692c196627790cdcb9e6bba1f
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-12-02 14:12:28 +0000

    Fri, Dec 2, 2022, 2:12 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,7 +1,9 @@
 # TODO
+import
 text = input("Text: ")
 words = 1
 sentences = 1
+letters = 0
 i = 0
 while true:
     if text[i] == '\0':
@@ -10,5 +12,14 @@ while true:
         words = words + 1
     if text[i] == '.' and text[i] == '?'and text[i] == '!':
         sentences = sentences +  1
+    if text[i] ==
+        letters = letters + 1
     i = i + 1
+
+index = 0.0588 * l - 0.296 * s - 15.8
+if index < 1.5:
+    print("Before Grade 1")
+elif index >= 15.5:
+    print("Grade 16+")
+elif in
 
