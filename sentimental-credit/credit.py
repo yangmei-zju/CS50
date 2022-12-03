efc37d53102050154ec4493c47ad7fcaa2e1e6ad
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-12-03 00:59:41 +0000

    Sat, Dec 3, 2022, 12:59 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,20 +4,19 @@ while True:
     number = input("Number: ")
     if int(number) >= 0:
         break
-i = 1
+length = 1
 for d in number:
     n = int(d)
-    if i // 2 != 0:
+    if length // 2 != 0:
         if n < 5:
             sum1 = sum1 + 2 * n
         else:
             sum1 = sum1 + 2 * n // 10 + 2 * n / 10
     else :
-        if n < 5:
-            sum2 = sum2 + 2 * n
-        else :
-            sum2 = sum2 + 2 * n // 10 + 2 * n / 10
+        sum2 = sum2 + 2 * n
 
+length = length - 1
+sum = sum1 + sum2
 
 
 
