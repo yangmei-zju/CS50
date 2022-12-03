e3013aa8990ef60f7f4c7cfa362ca47d2a2659a3
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-12-03 01:57:52 +0000

    Sat, Dec 3, 2022, 1:57 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -7,18 +7,26 @@ while True:
 length = 1
 for d in number:
     n = int(d)
-    if length // 2 != 0:
+    if length % 2 != 0:
         if n < 5:
             sum1 = sum1 + 2 * n
         else:
-            sum1 = sum1 + 2 * n // 10 + 2 * n / 10
+            sum1 = sum1 + 2 * n % 10 + 2 * n / 10
     else :
         sum2 = sum2 + 2 * n
 
 length = length - 1
 sum = sum1 + sum2
-if sum // 10 == 0:
+if sum % 10 == 0:
     while b > 100:
-        b = b / 10
+        b = b % 10
 
+    if (b == 34 or b == 37) and length == 15:
+        print("AMEX")
+    elif b >= 51 and b <= 55 and length == 16:
+        print("MASTERCARD")
+    elif (b >= 40 and b <= 49) and (length == 13 or length == 16):
+        print("VISA")
+    else:
+        print("INVALID")
 
