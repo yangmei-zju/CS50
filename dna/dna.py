6ace864597482e4166bf5bf618cd2fb21a2e19ed
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-12-03 16:25:55 +0000

    Sat, Dec 3, 2022, 4:25 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -13,14 +13,17 @@ def main():
         reader = csv.DictReader(file1)
         for row in reader:
             database.append(row)
+
     # TODO: Read DNA sequence file into a variable
-    sequences = []
+    sequence = []
     with open(sys.argv[2], "r") as file2:
-        
-
-
+        reader = csv.reader(file2)
+        for string in reader:
+            for character in string[0]:
+                sequence.append(character)
 
     # TODO: Find longest match of each STR in DNA sequence
+
 
     # TODO: Check database for matching profiles
 
