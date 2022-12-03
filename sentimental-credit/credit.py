a498012e05d8b5d94375ce24fe80eb4a0ae469dd
Freedom-zju <110583176+Freedom-zju@users.noreply.github.com>
2022-12-03 02:34:10 +0000

    Sat, Dec 3, 2022, 2:34 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -9,14 +9,14 @@ for d in number:
     n = int(d)
     if length % 2 != 0:
         if n < 5:
-            sum1 = sum1 + 2 * n
+            sum = sum + 2 * n
         else:
-            sum1 = sum1 + 2 * n % 10 + 2 * n // 10
+            sum = sum + 2 * n % 10 + 2 * n // 10
     else :
-        sum2 = sum2 + 2 * n
+        sum = sum + 2 * n
 
 length = length - 1
-sum = sum1 + sum2
+
 if sum % 10 == 0:
     while True:
         b = b % 10
